a40172135bf1a180ecafeff1ebed3ab7725f69dd
TibyanSaad <saad.tibyan02@gmail.com>
2026-04-14 15:19:05 +0400

    added mazerederer code

--- src/MazeRenderer.java
+++ src/MazeRenderer.java
@@ -1,2 +1,37 @@
+
+import java.util.ArrayList;
+
+
 public class MazeRenderer {
+    static ArrayList<String> pathCoordinates = new ArrayList<>();
+
+    public static void printMaze() {
+        mazeArray2D[r][c] = '@';
+        for (char[] maze : mazeArray2D) {
+            System.out.println(maze);
+        }
+        for (int space = 0; space < 4; space++) {
+            System.out.println();
+        }
+    }
+
+    public static void mazeDelay() {
+        mazeArray2D[r][c] = '-';
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void symbolCoordinates() {
+        pathCoordinates.add("(" + r + "," + c + ") ");
+    }
+
+    public static void printPath() {
+        for (String path : pathCoordinates) {
+            System.out.print(path + " ");
+        }
+        System.out.println();
+    }
 }
